c0e07a5c84df9446bbb07929ffa207881c8e8e66
Горбулёв Сергей <gorbulevsv@yandex.ru>
2026-07-13 00:51:12 +0300

    докер на виртуальной машине

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -1,7 +1,8 @@
 import { Client as MinIOClient } from 'minio';
 
 export const minioClient = new MinIOClient({
-	endPoint: 'localhost', // важно: localhost для кода на хосте
+	endPoint: '192.168.0.107', // для виртуальной машины
+	//endPoint: 'localhost', // для локальной машины
 	port: 9000,
 	useSSL: false,
 	accessKey: 'minioadmin', // замените на ваши значения
